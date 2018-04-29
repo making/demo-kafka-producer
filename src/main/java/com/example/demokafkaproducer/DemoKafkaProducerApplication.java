fc1415228a32f994e13d8c11fde259ecfdbbb486
Toshiaki Maki <tmaki@pivotal.io>
2018-04-30 02:05:33 +0900

    Switch to Spring WebFlux

--- src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
+++ src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -25,9 +23,9 @@ public class DemoKafkaProducerApplication {
     }
 
     @PostMapping(path = "/messages")
-    public CompletableFuture<String> sendMessage(@RequestBody String message) {
-        CompletableFuture<SendResult<Object, String>> future = this.kafkaTemplate.send(topic, message).completable();
-        return future.thenApply(result -> result.getProducerRecord().toString());
+    public Mono<String> sendMessage(@RequestBody String message) {
+        return Mono.fromFuture(this.kafkaTemplate.send(topic, message).completable()) //
+                .map(result -> result.getProducerRecord().toString());
     }
 
 
