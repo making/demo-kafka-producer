59a71e0dd233b59599fedf0ea845986ea17cfb5e
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 02:55:58 +0900

    Update demo

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# Spring Kafka Demo for [CloudKarafka](https://www.cloudkarafka.com/)
-
-configure
-
-```
-CLOUDKARAFKA_USERNAME=...
-CLOUDKARAFKA_PASSWORD=...
-SPRING_KAFKA_BOOTSTRAP_SERVERS=...
-```
+# Spring Kafka Demo (Producer)
--- src/test/java/com/example/demokafkaproducer/DemoKafkaProducerApplicationTests.java
+++ src/test/java/com/example/demokafkaproducer/DemoKafkaProducerApplicationTests.java
@@ -1,16 +1,14 @@
 package com.example.demokafkaproducer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class DemoKafkaProducerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
