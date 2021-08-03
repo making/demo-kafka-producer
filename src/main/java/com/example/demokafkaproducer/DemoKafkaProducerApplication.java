1ead6e7cc96f1eda3c24329f3f23f3bda4d628fa
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 12:50:43 +0900

    Use CompletionStage instead of Mono

--- src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
+++ src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
@@ -1,5 +1,7 @@
 package com.example.demokafkaproducer;
 
+import java.util.concurrent.CompletionStage;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,29 +9,29 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
 public class DemoKafkaProducerApplication {
-    private final String topic;
-    private final KafkaTemplate<Object, String> kafkaTemplate;
+	private final String topic;
 
-    public DemoKafkaProducerApplication(
-            @Value("${sample.topic}")
-                    String topic, KafkaTemplate<Object, String> kafkaTemplate) {
-        this.topic = topic;
-        this.kafkaTemplate = kafkaTemplate;
-    }
+	private final KafkaTemplate<Object, String> kafkaTemplate;
 
-    @PostMapping(path = "/messages")
-    public Mono<String> sendMessage(@RequestBody String message) {
-        return Mono.fromFuture(this.kafkaTemplate.send(topic, message).completable()) //
-                .map(result -> result.getProducerRecord().toString());
-    }
+	public DemoKafkaProducerApplication(
+			@Value("${sample.topic}")
+					String topic, KafkaTemplate<Object, String> kafkaTemplate) {
+		this.topic = topic;
+		this.kafkaTemplate = kafkaTemplate;
+	}
 
+	@PostMapping(path = "/messages")
+	public CompletionStage<String> sendMessage(@RequestBody String message) {
+		return this.kafkaTemplate.send(topic, message)
+				.completable()
+				.thenApply(x -> x.getProducerRecord().value());
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoKafkaProducerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaProducerApplication.class, args);
+	}
 }
