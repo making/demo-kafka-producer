fdf5a27d09e5661ec432f4944950b73f64579da2
Toshiaki Maki <tmaki@vmware.com>
2021-08-03 16:10:55 +0900

    Reactive version

--- src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
+++ src/main/java/com/example/demokafkaproducer/DemoKafkaProducerApplication.java
@@ -1,11 +1,16 @@
 package com.example.demokafkaproducer;
 
-import java.util.concurrent.CompletionStage;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.RecordMetadata;
+import reactor.core.publisher.Mono;
+import reactor.kafka.sender.SenderOptions;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoKafkaProducerApplication {
 	private final String topic;
 
-	private final KafkaTemplate<Object, String> kafkaTemplate;
+	private final ReactiveKafkaProducerTemplate<Object, String> kafkaTemplate;
 
-	public DemoKafkaProducerApplication(@Value("${sample.topic}") String topic, KafkaTemplate<Object, String> kafkaTemplate) {
+	public DemoKafkaProducerApplication(@Value("${sample.topic}") String topic, KafkaProperties properties) {
 		this.topic = topic;
-		this.kafkaTemplate = kafkaTemplate;
+		this.kafkaTemplate = new ReactiveKafkaProducerTemplate<>(SenderOptions.create(properties.buildProducerProperties()));
 	}
 
 	@PostMapping(path = "/messages")
-	public CompletionStage<String> sendMessage(@RequestBody String message) {
+	public Mono<Map<String, ?>> sendMessage(@RequestBody String message) {
 		return this.kafkaTemplate.send(topic, message)
-				.completable()
-				.thenApply(x -> x.getProducerRecord().value());
+				.map(result -> {
+					final RecordMetadata recordMetadata = result.recordMetadata();
+					return Map.of(
+							"topic", recordMetadata.topic(),
+							"partition", recordMetadata.partition(),
+							"offset", recordMetadata.offset());
+				});
 	}
 
 	public static void main(String[] args) {
